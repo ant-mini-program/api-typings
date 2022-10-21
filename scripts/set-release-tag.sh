6f57acfd3dd9a224afc7231490b245809b02509a
野声 <lijiacheng.ljc@antgroup.com>
2022-10-21 18:42:22 +0800

    chore: update release script

--- scripts/set-release-tag.sh
+++ scripts/set-release-tag.sh
@@ -1,2 +1,3 @@
+export V=$(cat lerna.json| jq .version -r)
 # 让 latest 都打上 release 的 dist-tag
-npx lerna exec --no-bail --no-private --no-sort --stream -- '[ -n "$(npm v . dist-tags.latest)" ] && npm --registry https://registry.npmjs.org/ dist-tag add ${LERNA_PACKAGE_NAME}@$(npm v . dist-tags.latest) release'
+npx lerna exec --no-bail --no-private --no-sort --stream -- '[ -n $V ] && npm --registry https://registry.npmjs.org/ dist-tag add ${LERNA_PACKAGE_NAME}@$V release'
