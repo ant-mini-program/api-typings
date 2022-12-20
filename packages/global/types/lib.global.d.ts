7a5a6075f1961fbd0dd8c4e405f4b5e742a95b25
temu.psc <temu.psc@alipay.com>
2022-12-20 12:10:43 +0800

    optimize

--- packages/global/types/lib.global.d.ts
+++ packages/global/types/lib.global.d.ts
@@ -8,25 +8,9 @@ export interface IRequirePlugin<
 
 declare global {
   /**
-   * 使用插件提供的 JS 接口，函数返回值为 \`插件\` 通过 \`main\` 字段暴露的 JS 接口。
+   * Extra `this` for Component instance.
    */
-  const requirePlugin: IRequirePlugin;
-  /**
-   * Extra `this` for App instance.
-   */
-  interface IGlobalMiniProgramExtraThis4App {}
-
-  /**
-   * App's constructor
-   * @link https://opendocs.alipay.com/mini/framework/app-detail
-   */
-  let App: MiniProgram.App.Constructor;
-
-  /**
-   * Get App.
-   * @link https://opendocs.alipay.com/mini/framework/get-app
-   */
-  let getApp: MiniProgram.App.GetApp;
+  interface IGlobalMiniProgramExtraThis4Component {}
 
   /**
    * Extra `this` for Page instance.
@@ -34,32 +18,50 @@ declare global {
   interface IGlobalMiniProgramExtraThis4Page {}
 
   /**
+   * Extra `this` for App instance.
+   */
+  interface IGlobalMiniProgramExtraThis4App {}
+
+  /**
+   * 使用插件提供的 JS 接口，函数返回值为 \`插件\` 通过 \`main\` 字段暴露的 JS 接口。
+   */
+  const requirePlugin: IRequirePlugin;
+
+  /**
+   * App's constructor
+   * @link https://opendocs.alipay.com/mini/framework/app-detail
+   */
+  const App: MiniProgram.App.Constructor;
+
+  /**
+   * Get App.
+   * @link https://opendocs.alipay.com/mini/framework/get-app
+   */
+  const getApp: MiniProgram.App.GetApp;
+
+  /**
    * Get Current Pages
    * @link https://opendocs.alipay.com/mini/framework/getcurrentpages
    */
-  let getCurrentPages: MiniProgram.Page.GetCurrentPages;
+  const getCurrentPages: MiniProgram.Page.GetCurrentPages;
 
   /**
    * Page's constructor
    * @link https://opendocs.alipay.com/mini/framework/page-detail
    */
-  let Page: MiniProgram.Page.Constructor
-
-  /**
-   * Extra `this` for Component instance.
-   */
-  interface IGlobalMiniProgramExtraThis4Component {}
+  const Page: MiniProgram.Page.Constructor
 
   /**
    * Component's constructor
    * @link https://opendocs.alipay.com/mini/framework/component_object
    */
-  let Component: MiniProgram.Component.Constructor;
+  const Component: MiniProgram.Component.Constructor;
 
   /**
    * 注册一个 `mixin`，接受一个 `Object` 类型的参数。
-   * 
+   * @version 2.8.2
+   * @link https://opendocs.alipay.com/mini/05bchn
    */
-  let Mixin: MiniProgram.Mixin.Constructor
+  const Mixin: MiniProgram.Mixin.Constructor
 }
 
