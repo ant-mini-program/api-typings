17a3270ae92be95377dab208f8e0df4655d7627c
temu.psc <temu.psc@alipay.com>
2022-12-22 15:53:01 +0800

    查缺补漏 Page

--- packages/global/types/lib.page.d.ts
+++ packages/global/types/lib.page.d.ts
@@ -1,4 +1,100 @@
 declare namespace MiniProgram.Page {
+  interface IOnResizeEvent {
+    /**
+     * @summary 窗口尺寸
+     */
+    size: {
+      /**
+       * @summary 窗口宽度
+       */
+      windowWidth: number;
+      /**
+       * @summary 窗口高度
+       */
+      windowHeight: number;
+    }
+  }
+  interface IKeyboardHeightEvent {
+    /**
+     * @summary 键盘高度
+     */
+    height: number
+  }
+  interface ITabItemTapEvent {
+    /**
+     * @summary 点击来源。
+     */
+    from: string,
+    /**
+     * @summary 被点击 tabItem 的页面路径。
+     */
+    pagePath: string,
+    /**
+     * @summary 被点击 tabItem 的按钮文字。
+     */
+    text: string,
+    /**
+     * @summary 被点击 tabItem 的序号，从 0 开始。
+     */
+    index: number,
+  }
+  interface ISelectedTabItemTapEvent {
+    /**
+     * @summary 被点击 tabItem 的页面路径。
+     */
+    pagePath: string,
+    /**
+     * @summary 被点击 tabItem 的按钮文字。
+     */
+    text: string,
+    /**
+     * @summary 被点击 tabItem 的序号，从 0 开始。
+     */
+    index: number,
+  }
+
+  interface IPageScrollEvent {
+    /**
+     * @summary 页面滚动距离。
+     */
+    scrollTop: number;
+    /**
+     * @summary 页面内容高度。
+     */
+    scrollHeight: number;
+  }
+
+  interface IPullDownRefreshEvent {
+    /**
+     * @summary 触发原
+     * @description
+     * - manual: 下拉事件通过 my.startPullDownRefresh 触发
+     * - code: 下拉事件用通过户下拉触发
+     */
+    from: 'manual' | 'code',
+  }
+  interface IShareAppMessageEvent {
+    /**
+     * @summary 触发来源
+     * @description
+     * - button 页面分享按钮触发
+     * - menu 右上角分享按钮触
+     * - code 执行 my.showSharePanel 触发
+     * @version 1.10.0
+     */
+    from: 'button' | 'menu' | 'code',
+    /**
+     * @summary 如果 from 值为 button，则 target 为触发这次分享的 button，否则为 undefined。
+     * @version 1.10.0
+     */
+    target: object,
+    /**
+     * @summary 页面中包含 web-view 组件时，返回当前 web-view 的 URL。
+     * @version 1.6.0
+     */
+    webViewUrl: string,
+  }
+
   interface IShareAppMessage {
     title: string;
     desc: string;
@@ -10,10 +106,12 @@ declare namespace MiniProgram.Page {
   interface Events {
     /**
      * 页面加载时触发
+     * @param query 打开当前页面路径中的参数
+     * @see https://opendocs.alipay.com/mini/03durs
      */
     onLoad<Query = {}>(query: Query): void;
     /**
-     * 页面显示时触发
+     * 页面显示时/切入前台时触发
      */
     onShow(): void;
     /**
@@ -21,7 +119,7 @@ declare namespace MiniProgram.Page {
      */
     onReady(): void;
     /**
-     * 页面隐藏时触发
+     * 页面隐藏时/切入后台时触发
      */
     onHide(): void;
     /**
@@ -30,64 +128,63 @@ declare namespace MiniProgram.Page {
      */
     onUnload(): void;
     /**
-     * 页面返回时触发
-     * @since 1.13.7
-     */
-    onBack(): void;
-    /**
-     * 键盘高度变化时触发
-     * @since 1.13.7
-     */
-    onKeyboardHeight(): void;
-    /**
-     * 点击导航栏额外图标触发
-     * @see 设置额外图标: https://opendocs.alipay.com/mini/api/optionmenu
-     * @since 1.13.7
-     */
-    onOptionMenuClick(): void;
-    /**
-     * 点击右上角通用菜单中的自定义菜单按钮触发
-     * @since 1.13.7
-     */
-    onPopMenuClick(): void;
-    /**
-     * 下拉截断时触发
-     * @since 1.13.7
-     */
-    onPullIntercept(): void;
-    /**
-     * 页面下拉时触发
-     * @since 1.13.7
-     */
-    onPullDownRefresh(res: { from: 'manual' | 'code' }): void;
-    /**
      * 点击标题触发
-     * @since 1.13.7
      */
     onTitleClick(): void;
     /**
-     * 点击非当前 tabItem 后触发
-     * @since 1.13.7
+     * 点击导航栏额外图标触发
+     * @see 设置额外图标: https://opendocs.alipay.com/mini/api/optionmenu
      */
-    onTabItemTap(): void;
+    onOptionMenuClick(): void;
+    /**
+     * @summary 页面下拉时触发
+     * @description 需要先在 app.json 的 window 选项中开启 pullRefresh 。当处理完数据刷新后，my.stopPullDownRefresh 可以停止当前页面的下拉刷新。
+     * @param event 页面下拉事件      
+     * @see https://opendocs.alipay.com/mini/framework/page-detail#onPullDownRefresh(%7Bfrom%3A%20manual%7Ccode%7D)     
+     */
+    onPullDownRefresh(event: IPullDownRefreshEvent): void;
+    /**
+     * @summary 点击 tabItem（非当前） 时触发
+     * @param event 点击 tabItem（非当前）事件
+     */
+    onTabItemTap: (event: ITabItemTapEvent) => void;
+    /**
+     * @summary 页面滚动时触发。
+     * @param event 页面滚动事件
+     */
+    onPageScroll: (event: IPageScrollEvent) => void;
+    /**
+     * 页面被拉到底部时触发
+     * @description
+     * - onReachBottom() 上拉触底时才会触发，如果页面已经在页面底部继续上拉是不会再次触发。
+     * - 可配合 my.pageScrollTo 向上滚动一点位置或者在底部增加数据等方式让页面不处在底部位置达到可以连续上拉触发 onReachBottom()的效果。
+     * - 可在应用配置（app.json）通过 onReachBottomDistance 配置项 自定义 页面上拉触底时触发时距离页面底部的距离，单位为 px。
+     */
+    onReachBottom(): void;
     /**
      * 点击非当前 tabItem 前触发
-     * @since 1.13.7
      */
     beforeTabItemTap(): void;
     /**
+     * 键盘高度变化时触发
+     * @param event 键盘高度变化事件
+     */
+    onKeyboardHeight(event: IKeyboardHeightEvent): void;
+    /**
+     * @summary 导航栏左侧返回按钮(以及 Android 系统返回键）被点击时触发
+     */
+    onBack: () => void,
+    /**
      * window尺寸改变时触发
-     * @since 1.16.0
+     * @version 1.16.0
      */
-    onResize(res: { windowWidth: number; windowHeight: number }): void;
+    onResize(event: IOnResizeEvent): void;
     /**
-     * 页面滚动时触发
+     * @summary 点击当前 tabItem 后触发。
+     * @param event 点击当前 tabItem 事件
+     * @version 2.7.2
      */
-    onPageScroll(res: { scrollTop: number; scrollHeight: number }): void;
-    /**
-     * 页面被拉到底部时触发
-     */
-    onReachBottom(): void;
+    onSelectedTabItemTap: (event: ISelectedTabItemTapEvent) => void,
   }
 
   /**
@@ -101,10 +198,12 @@ declare namespace MiniProgram.Page {
     data: Data | (() => Data);
     /**
      * 页面加载时触发
+     * @param query 打开当前页面路径中的参数
+     * @see https://opendocs.alipay.com/mini/03durs
      */
     onLoad<Query = {}>(query: Query): void;
     /**
-     * 页面显示时触发
+     * 页面显示时/切入前台时触发
      */
     onShow(): void;
     /**
@@ -112,29 +211,63 @@ declare namespace MiniProgram.Page {
      */
     onReady(): void;
     /**
-     * 页面隐藏
+     * 页面隐藏时/切入后台时触发
      */
     onHide(): void;
     /**
-     * 页面被关闭
+     * 页面卸载时触发
      */
     onUnload(): void;
     /**
-     * 标题被点击
+     * @summary 在 Page 中定义 onShareAppMessage 函数，设置该页面的分享信息。
+     * @description
+     * 开发者可通过传入参数自定义小程序分享内容（例如：标题、描述、图片），用户通过点击或者复制分享的内容可以快速打开小程序，进入指定页面。目前支持的分享渠道有：支付宝朋友动态、支付宝好友、钉钉、新浪微博、微信、QQ。
+     * - 每个 Page 页面的右上角菜单中默认有 分享 按钮。
+     * - 用户点击分享按钮的时候会调用。
+     * - 要求返回一个对象（Object）类型，用于自定义该页面的分享信息（如果未定义该回调，会使用默认分享信息，不影响分享功能）。
+     * - 若定义该回调，但未返回对象（Object）类型，会导致默认分享不可用。
+     * - 分享图片中的二维码的有效期为 60 天，若需要长期有效的二维码，请登录 开放平台控制台 > 我的应用 > 进入小程序应用详情页 > 小程序码 中生成。
+     * - 小程序在 1.1.0 版本中开始支持 open-type 为 share 的按钮触发分享。
+     * - 从基础库 1.24.13、2.6.7 版本开始支持 async 写法，可通过 my.canIUse('page.onShareAppMessage.async') 检测是否支持。
+     * @see https://opendocs.alipay.com/mini/framework/page-detail#onShareAppMessage(options%3A%20Object)
+     */
+    onShareAppMessage(event: IShareAppMessageEvent): IShareAppMessage | Promise<IShareAppMessage>;
+    /**
+     * 点击标题触发
      */
     onTitleClick(): void;
     /**
-     * 页面被下拉
+     * 点击导航栏额外图标触发
+     * @see 设置额外图标: https://opendocs.alipay.com/mini/api/optionmenu
+     * @version 1.3.0
      */
-    onPullDownRefresh(): void;
+    onOptionMenuClick: () => void,
+    /**
+     * @summary 页面下拉时触发
+     * @description 需要先在 app.json 的 window 选项中开启 pullRefresh 。当处理完数据刷新后，my.stopPullDownRefresh 可以停止当前页面的下拉刷新。
+     * @param event 页面下拉事件      
+     * @see https://opendocs.alipay.com/mini/framework/page-detail#onPullDownRefresh(%7Bfrom%3A%20manual%7Ccode%7D)     
+     */
+    onPullDownRefresh(event: IPullDownRefreshEvent): void;
+    /**
+     * @summary 点击 tabItem（非当前） 时触发
+     * @param event 点击 tabItem（非当前）事件
+     * @version 1.11.0
+     */
+    onTabItemTap: (event: ITabItemTapEvent) => void;
+    /**
+     * @summary 页面滚动时触发。
+     * @param event 页面滚动事件
+     */
+    onPageScroll: (event: IPageScrollEvent) => void;
     /**
      * 页面被拉到底部时触发
+     * @description
+     * - onReachBottom() 上拉触底时才会触发，如果页面已经在页面底部继续上拉是不会再次触发。
+     * - 可配合 my.pageScrollTo 向上滚动一点位置或者在底部增加数据等方式让页面不处在底部位置达到可以连续上拉触发 onReachBottom()的效果。
+     * - 可在应用配置（app.json）通过 onReachBottomDistance 配置项 自定义 页面上拉触底时触发时距离页面底部的距离，单位为 px。
      */
     onReachBottom(): void;
-    /**
-     * 返回自定义分享信息
-     */
-    onShareAppMessage(): IShareAppMessage;
     /**
      * 事件处理函数对象
      * @version 1.13.7
